1fd88372d955f40683a1e9339676030041451bac
Dylan Miller <dylanmiller071@gmail.com>
2021-09-30 20:56:33 -0400

    Test2

--- ComponentFramework21.5.0/ComponentFramework/Main.cpp
+++ ComponentFramework21.5.0/ComponentFramework/Main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "SceneManager.h"
 #include "Debug.h"
-
+///////////////////////////////
   
 int main(int argc, char* args[]) {
 	static_assert(sizeof(void*) == 4, "This program is not ready for 64-bit build");
