f0fd1efc87a3eef37207e1f93e6443302b5b362b
dylanafm <60159313+dylanafm@users.noreply.github.com>
2021-09-30 23:42:38 -0400

    Test

--- SDL_Project20.1.8/SDL_Project/Main.cpp
+++ SDL_Project20.1.8/SDL_Project/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "GameManager.h"
-
+/// <summary>
+/// ////
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="args"></param>
+/// <returns></returns>
 
 int main(int argc, char* args[]) { /// Standard C-style entry point, you need to use it
 
